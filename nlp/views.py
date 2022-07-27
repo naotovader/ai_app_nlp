196630e1a8b99953c9536e6d085af4abf600ee25
naotovader <gallardo.st62@gmail.com>
2022-07-27 22:30:17 +0900

    first commit

---
+++ nlp/views.py
@@ -0,0 +1,31 @@
+from cgitb import html
+from unicodedata import category
+from django.shortcuts import render
+import pandas as pd
+import pickle
+
+from scipy import fft
+
+category_data = pd.read_csv("idx2category.csv")
+idx2category = {row.k: row.v for idx, row in category_data.iterrows()}
+print(idx2category)
+with open("rdmf.pickle", mode="rb") as ff:
+  model = pickle.load(ff)
+
+def index(request):
+  if request.method == "GET":
+    return render(
+      request,
+      "nlp/home.html"
+    )
+  else:
+    title = [request.POST["title"]]
+    print("title:", title)
+    result = model.predict(title)[0]
+    print("result:", result)
+    pred = idx2category[result]
+    return render(
+      request,
+      "nlp/home.html",
+      {"title": pred}
+    )
